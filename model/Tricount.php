d5680be741bd3c0f4e7c6a48affd818daed9a413
Senol Kacar <kacar.senol@gmail.com>
2022-12-06 17:15:06 +0100

    feat(Model): création du model User et Tricount

---
+++ model/Tricount.php
@@ -0,0 +1,59 @@
+<?php
+Class Tricount extends Model{
+    public function __construct(public string $title,public ?string $description,public DateTime $created_at,public int $creator)
+    {
+        
+    }
+
+    public function get_tricounts(): array{
+        $query = self::execute("SELECT * FROM tricounts",[]);
+        $data = $query->fetchAll();
+        $tricounts = [];
+        foreach($data as $row){
+            $tricounts[] = new Tricount($row["title"],$row["description"],$row["created_at"],$row["creator"]);
+        }
+        return $tricounts;
+    }
+
+    public function get_tricount_by_id(int $id): Tricount|false{
+        $query = self::execute("SELECT * FROM tricounts WHERE id = :id",[":id" => $id]);
+        $data = $query->fetch();
+        if($query->rowCount()==0){
+            return false;
+        }else{
+            return new Tricount($data["title"],$data["description"],$data["created_at"],$data["creator"]);
+        }
+    }
+
+    public function get_tricount_by_creator(int $creator): Tricount|false{
+        $query = self::execute("SELECT * FROM tricounts WHERE creator = :creator",[":creator" => $creator]);
+        $data = $query->fetch();
+        if($query->rowCount()==0){
+            return false;
+        }else{
+            return new Tricount($data["title"],$data["description"],$data["created_at"],$data["creator"]);
+        }
+    }
+
+    public function validate_title(string $title): array{
+        $errors=[];
+        if(!strlen($title)>3){
+            $errors[] = "Title must be at least 3 characters long";
+        }
+        return $errors;
+    }
+
+    public function validate_description(string $description): array{
+        $errors=[];
+        if(strlen($description)>0&&strlen($description)<3){
+            $errors[] = "Description must be at least 3 characters long";
+        }
+        return $errors;
+    }
+}
+
+
+
+
+
+?>
---
+++ model/User.php
@@ -0,0 +1,100 @@
+<?php
+Class User extends Model{
+    public function __construct(public string $mail,public string $hashed_password, public string $full_name, public string $role, public ?string $iban = null){}
+    
+    public function getUserByMail(string $mail): User|false{
+        $query = self::execute("SELECT * FROM users WHERE mail = :mail",[":mail" => $mail]);
+        $data = $query->fetch();
+        if($query->rowCount()==0){
+            return false;
+        }else{
+            return new User($data["mail"],$data["hashed_password"],$data["full_name"],$data["role"],$data["iban"]);
+        }
+    }
+
+    public static function get_users(): array{
+        $query = self::execute("SELECT * FROM users",[]);
+        $data = $query->fetchAll();
+        $users = [];
+        foreach($data as $row){
+            $users[] = new User($row["mail"],$row["hashed_password"],$row["full_name"],$row["role"],$row["iban"]);
+        }
+        return $users;
+    }
+
+    public static function validate_email(string $mail): array{
+        $errors=[];
+        if(!strlen($mail)>0){
+            $errors[] = "Email is required";
+        }
+        if(!filter_var($mail,FILTER_VALIDATE_EMAIL)){
+            $errors[] = "Invalid email";
+        }
+        return $errors;
+    }
+
+    public static function validate_unicity(string $mail): array{
+        $errors=[];
+        $user = self::getUserByMail($mail);
+        if($user){
+            $errors[] = "Email already used";
+        }
+        return $errors;
+    }
+
+    public static function validate_password(string $password): array{
+        $errors=[];
+        if(strlen($password)<8||strlen($password)>16){
+            $errors[] = "Password length must be between 8 and 16 characters";
+        }
+        if(!((preg_match("/[A-Z]/",$password))&&preg_match("/\d/",$password)&&preg_match("/['\";:,.\/?!\\-]/",$password))){
+            $errors[] = "Password must contain at least one uppercase letter, one number and one special character";
+        }
+        return $errors;
+    }
+
+    public static function check_password(string $clear_password, string $hash): bool{
+        return $hash === Tools::my_hash($clear_password);
+    }
+
+    public static function validate_passwords(string $password, string $password_confirm): array{
+        $errors=User::validate_password($password);
+        if($password !== $password_confirm){
+            $errors[] = "Passwords don't match";
+        }
+        return $errors;
+    }
+
+    public static function validate_login(string $mail, string $password): array{
+        $errors = [];
+        $user = User::getUserByMail($mail);
+        if($user){
+            if(!self::check_password($password,$user->hashed_password)){
+                $errors[] = "Wrong password. Please try again";
+            }
+        }else{
+            $errors[] = "Can't find user with this email '$mail'. Please sign up";
+        }
+        return $errors;
+    }
+
+    public static function validate_full_name(string $full_name): array{
+        $errors=[];
+        if(!strlen($full_name)>3){
+            $errors[] = "Full name must be at least 3 characters long";
+        }
+        return $errors;
+    }
+
+    public static function validate_iban(string $iban): array{
+        $errors=[];
+    
+        if(!preg_match("/^[A-Z]{2}[0-9]{2}[A-Z0-9]{4}[0-9]{7}([A-Z0-9]?){0,16}$/",$iban)){
+            $errors[] = "Invalid IBAN";
+        }
+        return $errors;
+    }
+
+
+}
+?>
