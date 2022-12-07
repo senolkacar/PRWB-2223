b09ccc6060e772bd0f2e9e817281e956dc4590e3
Tingting Zh <zhoutingting1015@gmail.com>
2022-12-07 20:58:43 +0100

    feat: create model repartitions

---
+++ model/Repartitions.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "framework/Model.php";
+
+
+class Repartitions extends Model {
+
+    public function __construct(public int $operationId, public int  $userId, public int $weight  )  {   
+    }
+
+    //add
+
+    //delete
+
+
+
+}
