dfc9bf73381e5ad1d8c09afaf0dcffe0a27bb75c
Senol Kacar <kacar.senol@gmail.com>
2022-12-09 16:01:25 +0100

    feat(Login): amélioration de la vue login

--- view/view_login.php
+++ view/view_login.php
@@ -6,40 +6,41 @@
     <title>Tricount</title>
     <base href="<?= $web_root ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
 
 <body>
-    <div class="title">Sign in</div>
-    <div class="main">
-        <form action="main/login" method="post">
-        <table>   
-        <tr>
-                <td><label for="mail">Email</label></td>
-                <td><input type="email" name="mail" id="mail" required></td>
-        </tr>
-        <tr>
-                <td><label for="password">Password</label></td>
-                <td><input type="password" name="password" id="password" required></td>
-        </tr>       
-            </table> 
-                <input type="submit" value="Login">
-        </form>
-        <?php if (count($errors)!=0) : ?>
-            <div class='errors'>
-                <p>Please correct the following error(s):</p>
-                <ul>
-                    <?php foreach ($errors as $error) : ?>
-                        <li><?= $error ?></li>
-                    <?php endforeach; ?>
-                </ul>
+    <div class="login-form">
+    <form action="main/login" method="post">
+        <h2 class="text-center">Sign In</h2>
+        <div class="form-group">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">
+                        <span class="fa fa-user"></span>
+                    </span>                    
+                </div>
+                <input type="text" class="form-control" placeholder="Username" required="required">
             </div>
-        <?php endif; ?>
-    </div>
-    <div class="main">
-        <div class="input">
-            <a href="main/signup">Sign up</a>
-    </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">
+                        <i class="fa fa-lock"></i>
+                    </span>                    
+                </div>
+                <input type="password" class="form-control" placeholder="Password" required="required">
+            </div>
+        </div>        
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-block">Log in</button>
+        </div>
+        <p class="text-center">
+            <a href="main/signup" class="float-center">New here ? Click here to join the party <i class="fa fa-rocket"></i> !</a>
+        </p>
+    </form>
 </body>
-
 </html>
