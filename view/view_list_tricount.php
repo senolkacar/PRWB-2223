c7f09b7db73598ffe60c72e7c6a69c9367b35bf3
Tingting Zh <zhoutingting1015@gmail.com>
2022-12-21 21:31:06 +0100

    feat(list ticount):add css

--- view/view_list_tricount.php
+++ view/view_list_tricount.php
@@ -5,16 +5,22 @@
         <title>your tricounts</title>
         <base href="<?= $web_root ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         
       
     </head>
-    <body class="container">
-        <div class="title">your tricounts </div>
-        <a href="tricount/add_tricount"><button type="button" class="btn btn-primary btn-block,sticky-sm-top">Add</button></a>
-       
-        <div class="main">
+    <body >
+        <div  class="container p-3 mb-3 bg-info text-dark"> 
+            <div class="d-flex justify-content-between mb-3">
+                <div class="h2">Your tricounts </div>
+                <a href="tricount/add_tricount"><button type="button" 
+                class="btn btn-primary btn-block,sticky-sm-top,">Add</button></a>
+            </div>
+        </div>
+
+        <div class="container-sm">
             <ul class="list-group"> 
             <?php foreach ($tricounts as $tricount):  ?>
 
@@ -43,8 +49,16 @@
             </a>
                 </li>
             </ul>    
-        </div>     
-        <a href="user/settings">Settings</a>   
+        </div>           
+        
+        <br><br>
+        <div class="container-sm">
+        <a href="user/settings" class="float-end"> Settings
+        <i class="bi bi-gear-fill"></i>
+        </a>  
+        </div> 
+        
+       
 
 
         
