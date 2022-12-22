055c872962d9b3a4e07df2fb68ca5d31623d0741
Tingting Zh <zhoutingting1015@gmail.com>
2022-12-22 18:57:54 +0100

    feat(add tricount):add bootstrap

--- view/view_add_tricount.php
+++ view/view_add_tricount.php
@@ -2,41 +2,51 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Add Tricount</title>
         <base href="<?= $web_root ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+	integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
     </head>
     <body>
-    <nav class="navbar navbar-light bg-light">
-    <a class="navbar-brand" href="tricount/index">
-        <button type="button" class="btn btn-primary">Cancel</button>
-    </a>
-    <div class="title">Tricount  &#32 &#9654; &#32 Add </div>
-    
-   
-    </nav>
-        <div class="main">
+        <div  class="container p-3 mb-3 text-dark" style="background-color: #E3F2FD;">
+            <div class="d-flex justify-content-between mb-3">   
+                <a class="navbar-brand" href="tricount/index">
+                <button type="button" class="btn btn-outline-danger">Cancel</button>
+                </a>
+                <div class="h2">Tricount  &#32 <i class="bi bi-caret-right-fill"></i> &#32 Add </div>
+                <div class="h2"> </div>
+            </div>
+        </div>
+
+        <div class="container-sm">
             <form method='post' action='tricount/add_tricount' enctype='multipart/form-data'>
-                Title : <br>
-                <textarea name='title' id='title' rows='1'></textarea> <br>
-                Descripton (optional) : <br>
-                <textarea name='description' id='title' rows='3'></textarea> <br>
+                <div class="mb-3 mt-3">
+                    <label for='title'> Title : </label>
+                    <input type="text" class="form-control" name='title' id='title'>
+                 </div>
+                 <div class="mb-3 mt-3">
+                    <label for='description'> Descripton (optional) :  </label>
+                    <textarea class="form-control" name='description' id='title' rows='3'></textarea> <br>
+                 </div>
                 <button type="submit" class="btn btn-primary">Save</button>
             </form>
+        </div>
 
+        <div>
             <?php if (count($errors) != 0): ?>
                 <div class='errors'>
                     <p>Please correct the following error(s) :</p>
                     <ul>
                         <?php foreach ($errors as $error): ?>
-                            <li><?= $error ?></li>
+                            <li class="text-danger"><?= $error ?></li>
                         <?php endforeach; ?>
                     </ul>
                 </div>
             <?php endif; ?>
-
-
         </div>
     </body>
 </html>
