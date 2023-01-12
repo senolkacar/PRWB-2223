7a7781c8ff0e024cdc22ceeee9f38c040379c090
Senol Kacar <kacar.senol@gmail.com>
2023-01-12 14:12:01 +0100

    feat(show_tricount): modification mineur css

--- view/view_show_tricount.php
+++ view/view_show_tricount.php
@@ -15,7 +15,7 @@
         <div class="container p-3 mb-3 text-dark" style="background-color:#E3F2FD">
             <div class="d-flex justify-content-between">
             <a class="navbar-brand" href="tricount/index"><button type="button" class="btn btn-outline-danger">Back</button></a>
-            <div class="text-secondary mt-2"><?=$tricount->title?> &#32<i class="bi bi-caret-right-fill"></i> &#32 Expenses </div>
+            <div class="text-secondary fw-bold mt-2"><?=$tricount->title?> &#32<i class="bi bi-caret-right-fill"></i> &#32 Expenses </div>
             <a class="navbar-brand" href="tricount/edit_tricount/<?=$tricount->id; ?>"><button type="button" class="btn btn-primary">Edit</button></a>
             </div>
         </div>
