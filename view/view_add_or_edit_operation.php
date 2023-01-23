497990b7f9e9b1f293676762230b141d5017ef80
Senol Kacar <kacar.senol@gmail.com>
2023-01-23 16:47:07 +0100

    fix: typo style.css

--- view/view_add_or_edit_operation.php
+++ view/view_add_or_edit_operation.php
@@ -5,7 +5,7 @@
         <title><?=$page_title?></title>
         <base href="<?= $web_root ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
     <nav class="navbar navbar-light bg-light">
