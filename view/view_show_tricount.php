316f59847626a671acb78e10c60d0683752798da
Senol Kacar <kacar.senol@gmail.com>
2023-01-27 14:16:02 +0100

    feat(show_tricount): modification in regards of html validation

--- view/view_show_tricount.php
+++ view/view_show_tricount.php
@@ -13,30 +13,30 @@
         <header>
         <div class="container p-3 mb-3 text-dark" style="background-color:#E3F2FD">
             <div class="d-flex justify-content-between">
-            <a class="navbar-brand" href="tricount/index"><button type="button" class="btn btn-outline-danger">Back</button></a>
-            <div class="text-secondary fw-bold mt-2"><?=$tricount->title?> &#32<i class="bi bi-caret-right-fill"></i> &#32 Expenses </div>
-            <a class="navbar-brand" href="tricount/edit_tricount/<?=$tricount->id; ?>"><button type="button" class="btn btn-primary">Edit</button></a>
+            <a class="btn btn-outline-danger" href="tricount/index">Back</a>
+            <div class="text-secondary fw-bold mt-2"><?=$tricount->title?> &#32;<i class="bi bi-caret-right-fill"></i> &#32; Expenses </div>
+            <a class="btn btn-primary" href="tricount/edit_tricount/<?=$tricount->id; ?>">Edit</a>
             </div>
         </div>
         </header>
         <div class="container-sm">      
             <ul class="list-group align-items-center">
                     <?php if($nb_participants == 0): ?>
-                        <span class="m-3 border w-100 rounded">
+                        <li class="m-3 border w-100 rounded">
                         <div class="text-center">
                         <div class="h3 p-3 border-bottom border-secondary" style="background-color: #F7F7F7">You are alone!</div>
                         <div class="text p-3">Click below to add your friends!</div>
                         <a class="btn btn-primary mb-3" href="tricount/edit_tricount/<?=$tricount->id; ?>">Add Friends</a>
                         </div>
-                        </span>
+                    </li>
                         <?php elseif(!$depenses): ?>
-                        <span class="m-3 border border-secondary w-100 rounded">
+                        <li class="m-3 border border-secondary w-100 rounded">
                         <div class="text-center">
                         <div class="h3 p-3 border-bottom border-secondary" style="background-color: #F7F7F7"><?php echo "Your tricount is empty!" ?></div>
                         <div class="text p-3">Click below to add your first expense!</div>
                         <a class="btn btn-primary mb-3" href="operation/add_operation/<?=$tricount->id; ?>">Add an expense</a>   
                         </div>
-                        </span>
+                        </li>
             </ul>
                         <?php else: ?>
                         <a class="btn btn-success w-100 mb-3 p-2" href="tricount/show_balance/<?=$tricount->id; ?>"><i class="bi bi-arrow-left-right"></i> View Balance</a>   
@@ -50,7 +50,6 @@
                                 <?php echo "Paid by ".User::get_user_by_id($depense["initiator"])->full_name?>
                                 </div>  
                             </div>
-                            <br>
                             <div class="text-end">
                                 <p class="fw-bold"><?=round($depense["amount"],2)?>&euro;</p>
                                 <?=$depense["operation_date"]?></a>
@@ -58,9 +57,7 @@
                         </li>
                     </ul>
                         <?php endforeach; ?>
-                    <?php endif; ?>
-                    <br>
-                </ul>       
+                    <?php endif; ?>     
         </div>
         <footer class="footer mt-auto fixed-bottom">
         <div class="container p-1 text-dark" style="background-color:#E3F2FD">
