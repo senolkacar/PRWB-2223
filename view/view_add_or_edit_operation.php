ae73fcf6700977ddba7f2cb005e744087ef4ad6a
Tingting ZHOU <0209tizhou@student.epfc.eu>
2023-03-13 16:07:27 +0100

    feat(add_or_edit_operation):add amount

--- view/view_add_or_edit_operation.php
+++ view/view_add_or_edit_operation.php
@@ -8,6 +8,9 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
 	integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+        <script>
+            
+            </script>
     </head>
     <body>
     <header>
@@ -89,6 +92,7 @@
                                 <?php $weight = $weights[$i]; ?>
                             <?php endif; ?>
                         <?php endfor; ?>
+                        <input type="number" step="0.01" class="form-control mb-3" name ="amount[]" value= "">
                         <input type="number" class="form-control mb-3" name="weights[]" min="0" max="<?=$nb_subscriptions?>" value="<?=$weight?>">
                         <input type="hidden" name="ids[]" value="<?=$subscription->id?>">
                         </div>
