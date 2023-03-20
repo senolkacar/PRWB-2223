85455616ce4585226cbee624b2ae670be02cd84e
Senol Kacar <kacar.senol@gmail.com>
2023-03-20 23:16:21 +0100

    fix(view_add_tricount): transforming the ajax get to post in order to resolve the issue with title containing space

--- controller/ControllerTricount.php
+++ controller/ControllerTricount.php
@@ -23,11 +23,15 @@ class ControllerTricount extends MyController {
 
     public function tricount_exists_service(): void{
         $res = "false";
-        if(isset($_GET["param1"]) && $_GET["param1"]!=="" && isset($_GET["param2"]) && $_GET["param2"]!==""){
-            if(Tricount::title_creator_existe(User::get_user_by_id($_GET["param1"]),$_GET["param2"]))
+        $id = $_POST["creator"];
+        $title = $_POST["title"];
+        
+        if(isset(($id)) && isset($title)){
+            if(Tricount::title_creator_existe(User::get_user_by_id($id),$title)){
                 $res = "true";
         }
         echo $res; 
+    }
     }
 
     public function add_tricount():void {
